6d6bdc2783d566ffbb579638d2b59ed397bdf871
Vale <162757566+Sukiih@users.noreply.github.com>
2024-08-19 12:50:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    document.getElementById('surpriseMessage').classList.remove('hidden');
+    this.style.display = 'none';
+});
